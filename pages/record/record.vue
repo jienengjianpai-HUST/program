8764ef2843652c69edd45824769eb174507fed4f
BirB.yeah <1635036180@qq.com>
2023-01-08 20:05:25 +0800

    对一些文件进行了修改、配置

--- pages/record/record.vue
+++ pages/record/record.vue
@@ -3,10 +3,10 @@
 	<view class="record-container gradient"  style="position: fixed; top: 0; left: 0; right: 0; bottom: 0">
 		<view class="item">
 			<view class="item1">
-				<button class="button">出行</button>
+				<button class="button" @click="toGoOut">出行</button>
 			</view>
 			<view class="item2">
-				<button class="button">电子产品使用时间</button>
+				<button class="button" @click="toE_Product">电子产品使用时间</button>
 			</view>
 			<view class="item3">
 				<button class="button" @click="toTrash">生活垃圾</button>
@@ -26,6 +26,16 @@
 			toTrash(){
 				uni.navigateTo({
 					url:'/pages/record/trash/trash'
+				})
+			},
+			toGoOut () {
+				uni.navigateTo({
+					url:'/pages/record/GoOut/OutWaySelection/OutWaySelection'
+				})
+			},
+			toE_Product () {
+				uni.navigateTo({
+					url:'/pages/record/E-Product/E_ProductInput/E_ProductInput'
 				})
 			}
 		}
