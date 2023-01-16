1a912033a7b93c1a65a62dcd0cb07d20afbd229b
BirB.yeah <1635036180@qq.com>
2023-01-17 01:49:00 +0800

    创建了新的辅助注册云函数

---
+++ uniCloud-aliyun/cloudfunctions/user_register_secondary_info/index.js
@@ -0,0 +1,32 @@
+'use strict';
+const collection = uniCloud.database().collection("user")
+let ret_code = ""
+exports.main = async (event, context) => {
+	const {
+		student_id,
+		birthday,
+		sex
+	} = event
+	const search_res = await collection.where({
+		student_id:event.student_id
+	}).get()
+	console.log(search_res)
+	const data_id = search_res.data[0]._id
+	const set_res = await collection.doc(data_id).update({
+		birthday:birthday,
+		sex:sex
+	}).then(res => {
+		console.log("res")
+		console.log(res)
+		console.log(res.updated)
+		if (res.updated <= 1)
+		{
+			ret_code = "注册成功"
+		}
+	}).catch(err => {
+		console.log("err")
+		console.log(err)
+		ret_code = "注册失败"
+	})
+	return ret_code
+};
